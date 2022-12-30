b369ef2bf902c64f2ca5c1c77c4b8b7a393a3531
KarolisGib <karolisgibavicius@yahoo.com>
2022-12-30 16:53:32 +0200

    po pertraukos

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,32 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
-//        sumInt(5, 6);
-//        PISq();
-//        System.out.println(multip(5, 10));
+//    sumInt(5, 6);
 
+//    System.out.println(PISq());
 
-        int[] nums = rndIntArr(5);
-        System.out.println(nums);
+//    System.out.println(multip(5, 10));
+
+//    int[] arr = {1,2,3,4,5};
+//        arr1(arr);
+
+        int[] masyvas = rndArr();
+        for (int i = 0; i < masyvas.length; i++) {
+//            System.out.print(masyvas[i] + " ");
+        }
+
+//        System.out.println(sum(masyvas));
+
+//        System.out.println(med(masyvas));
+
+//    kvadratas(5, 3);
+        String tekstas = "Siandien labai grazi diena";
+//        count(tekstas);
+
+//    kodas("Karolis");
 
 
     }
@@ -21,7 +39,7 @@ public class Main {
 // uzduotis lengvesnis 2
 
     public static double PISq() {
-        return 3.14 * 3.14;
+        return 9.8596;
     }
 
 // uzduotis lengvesnis 3
@@ -32,38 +50,62 @@ public class Main {
 
 // uzduotis lengvesnis 4
 
-//    public static int[] arr1() {
-//        int[] a = new int[];
-//        for (int i = 0; i < int[le]; i++) {
-//            a[i];
-//        }
-//        return a;
-//    }
+    public static void arr1(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 
 // uzduotis lengvesnis 5
 
-    public static int[] rndIntArr(int length){
-        int[] nums = new int[length];
-        for (int i = 0; i < length; i++) {
-            nums[i] = (int) (Math.round(Math.random()));
+    public static int[] rndArr() {
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.round(Math.random() * 100));
         }
-        return nums;
+        return arr;
     }
 
 // uzduotis lengvesnis 6
+
+    public static int sum(int[] arr) {
+        return Arrays.stream(arr).sum();
+    }
+
 // uzduotis lengvesnis 7
+
+    public static double med(int[] arr) {
+        return sum(arr) / (arr.length * 1.0);
+    }
+
 // uzduotis lengvesnis 8
 
-    public static String kvadratas(int row, int column){
-    for (int i = 1; i < row; i++) {
+    public static String kvadratas(int row, int column) {
+        for (int i = 1; i <= row; i++) {
             System.out.println();
-        for (int j = 1; j < column; j++) {
+            for (int j = 1; j <= column; j++) {
                 System.out.print(" * ");
+            }
         }
-        }
+        return " * ";
     }
 // uzduotis lengvesnis 9
+
+    public static void count(String text) {
+        int txt = text.length();
+        int count = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isLetter(text.charAt(i))) {
+                count++;
+            }
+        }
+        System.out.println("Tekste raidziu yra: " + count + " o tarpu: " + (txt - count));
+    }
+
 // uzduotis lengvesnis 10
 
-
+    public static String kodas(String text) {
+        System.out.println(new StringBuilder(text).reverse());
+        return text;
+    }
 }
